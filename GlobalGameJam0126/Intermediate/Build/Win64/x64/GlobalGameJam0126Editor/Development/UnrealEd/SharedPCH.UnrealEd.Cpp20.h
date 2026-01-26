9465e278fdfca50282c328089810358f4a361744
JesseAdams313 <echodevgames@gmail.com>
2026-01-25 22:06:30 -0500

    New Folders and Maps Initialization

---
+++ GlobalGameJam0126/Intermediate/Build/Win64/x64/GlobalGameJam0126Editor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/GitRepos/GlobalGameJam01-25Repo/GlobalGameJam01-26/GlobalGameJam0126/Intermediate/Build/Win64/x64/GlobalGameJam0126Editor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
---
+++ GlobalGameJam0126/Source/GlobalGameJam0126/GlobalGameJam0126.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GlobalGameJam0126.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlobalGameJam0126, "GlobalGameJam0126" );
