cc5d4fa47f81f3d629392a5a9f7f8fd798cd3460
JesseAdams313 <echodevgames@gmail.com>
2026-01-25 21:37:32 -0500

    Initial Startup

---
+++ GlobaGameJam0126/Source/GlobaGameJam0126/GlobaGameJam0126.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GlobaGameJam0126.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlobaGameJam0126, "GlobaGameJam0126" );
